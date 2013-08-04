f9780f2dee83e99710e17490d6820fa1604b793b
Malte Schmitz <malte@schmitz-sh.de>
2013-08-05 00:41:17 +0200

    Adds license

--- gameoflife.py
+++ gameoflife.py
@@ -1,8 +1,30 @@
-# Game of Life in Python
+# Game of Life mit Python und Tk
+#
+# Dieser Quelltext ist frei unter der MIT-Lizenz.
+#
+# Copyright (c) 2013 Malte Schmitz
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
-# Für eine Schritt die Taste n oder die Leertaste drücken.
+# Für eine Schritt die Taste N oder die Leertaste drücken.
 # Zum Starten und Stoppen des automatischen Ablaufs die
-# Taste p drücken.
+# Taste P drücken.
 
 # Anzahl Zellen auf dem Bildschirm
 width = 40
